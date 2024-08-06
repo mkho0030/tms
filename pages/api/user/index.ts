3ea652febbc9c08b4142571c95343bab1c6f4156
sqooid <sqooiddev@gmail.com>
2024-08-06 20:59:56 +1000

    feat: combine user creaet and update operations

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -7,7 +7,7 @@ export default async function handler(
   // create user
   if (req.method === "POST") {
     const { uid, name, email, photoUrl } = JSON.parse(req.body);
-    //TODO: create user in db. don't bother checking for existing emails
+    //TODO: upsert user in db. don't bother checking for existing emails
     res.json({ uid, name, email, photoUrl });
   }
   // get user info, settings and notifications
@@ -15,11 +15,5 @@ export default async function handler(
     const { uid } = JSON.parse(req.body);
     //TODO: get user from db and return details
     res.json({ uid }); //TODO: add rest of fields from db
-  }
-  // update user info and settings
-  if (req.method === "PUT") {
-    const { uid, name, email, photoUrl } = JSON.parse(req.body);
-    //TODO: update user in db
-    res.json({ uid, name, email, photoUrl });
   }
 }
