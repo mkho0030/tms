10363d7f565dad25ef5c828c998935ca07330cd2
Ollie <144658936+olshissy@users.noreply.github.com>
2024-08-12 11:28:39 +1000

    TMS-24 Develop Teams Toolbar
    [Changes]:
    - Toolbar fills full screen
    - Avatar section is made into group
    
    [New]:
    - More buttons added to the toolbar
    - Dropdown boxes and search box added
    - Add task button added
    - Headers for task table added

--- components/Toolbar/index.tsx
+++ components/Toolbar/index.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import MenuIcon from '@mui/icons-material/Menu';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+import { AvatarGroup, Divider } from '@mui/material';
 
 const avatars = [
   '/static/images/avatar/1.jpg', 
@@ -21,18 +19,15 @@ const avatars = [
 
 function ResponsiveAppBar() {
   return (
-    <AppBar position="static" sx={{ backgroundColor: 'white', width:'100%'}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
+        <Toolbar disableGutters sx={{ backgroundColor: 'white', width:'100%'}}>
           <Typography
-            variant="h6"
+            variant="h4"
             noWrap
             component="div"
             sx={{
               flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
+              fontWeight: 400,
+              letterSpacing: '.022rem',
               color: 'black',
               textDecoration: 'none',
             }}
@@ -51,21 +46,34 @@ function ResponsiveAppBar() {
             </Button>
 
             <Box sx={{ display: 'flex', alignItems: 'center', ml: 2 }}>
-              {avatars.map((src, index) => (
-                <Avatar key={index} alt={`Member ${index + 1}`} src={src} sx={{ marginLeft: -1 }} />
-              ))}
+              <AvatarGroup max={4}>
+              <Avatar alt="Test" src="/static/images/avatar/1.jpg" />
+              <Avatar alt="Test" src="/static/images/avatar/2.jpg" />
+              <Avatar alt="Test" src="/static/images/avatar/3.jpg" />
+              <Avatar alt="Test" src="/static/images/avatar/4.jpg" />
+              <Avatar alt="Test" src="/static/images/avatar/5.jpg" />
+              </AvatarGroup>
             </Box>
 
-            <IconButton sx={{ ml: 1, color:"black"}}>
+            <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              border: '1px solid gray',
+              borderRadius: 1,
+              ml: 2,
+            }}
+          >
+            <IconButton sx={{ color: 'black' }}>
               <FormatAlignLeftIcon />
             </IconButton>
-            <IconButton  sx={{ ml: 2, color: "black" }}>
+            <Divider orientation="vertical" flexItem sx={{ backgroundColor: 'gray' }} />
+            <IconButton sx={{ color: 'black' }}>
               <CalendarTodayIcon />
             </IconButton>
           </Box>
+          </Box>
         </Toolbar>
-      </Container>
-    </AppBar>
   );
 }
 
