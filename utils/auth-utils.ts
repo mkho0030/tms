3ca1dc1c9e2e4a03ceb72ecafbff7cb929171667
sqooid <sqooiddev@gmail.com>
2024-08-18 22:30:59 +1000

    feat(api): TMS-22 make server auth skip uid configurable for more flexible testing

--- utils/auth-utils.ts
+++ utils/auth-utils.ts
@@ -3,7 +3,10 @@ import { NextApiRequest } from "next";
 
 export const getRequestUser = async (req: NextApiRequest) => {
   // in testing, return uid test
-  if (process.env.SKIP_SERVER_AUTH) return "test";
+  if (process.env.SKIP_SERVER_AUTH) {
+    const testUser = req.query.testUser;
+    return testUser?.toString() ?? "test";
+  }
 
   const token = req.cookies?.auth_token ?? "";
   if (!token) return null;
