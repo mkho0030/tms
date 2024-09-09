cfe5282fb3d65caf21c88384759f4dee221209b7
Lucas <git@sl.thesqooid.com>
2024-09-09 16:02:41 +1000

    test: only test on chrome browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,19 +34,19 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
+      name: "chrome",
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/integration/test-1.spec.ts
+++ tests/integration/test-1.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test("register", async ({ page }) => {
+// login in before all tests
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page.waitForURL("http://localhost:3000/auth/login");
   await page.getByLabel("Email *").click();
@@ -9,4 +10,15 @@ test("register", async ({ page }) => {
   await page.getByLabel("Password *").fill("password");
   await page.getByRole("button", { name: "Login" }).click();
   await page.waitForURL("http://localhost:3000/tasks");
+});
+
+test("create project", async ({ page }) => {
+  await page.getByRole("button", { name: "Create new Team" }).click();
+  await page.getByPlaceholder("Team Name").click();
+  await page.getByPlaceholder("Team Name").fill("Test Project");
+  await page.getByRole("button", { name: "Create new team" }).click();
+  await page.waitForURL("http://localhost:3000/teams/*-*-*-*-*");
+  // await expect(
+  //   page.getByRole("button", { name: "Test Project", exact: true })
+  // ).toBeVisible();
 });
