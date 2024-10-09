a362cc004cd526ae799759608f6ff9b2cd9d2c43
Lucas <git@sl.thesqooid.com>
2024-10-09 14:24:30 +1100

    test: add rest of integration tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests/integration",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -23,13 +23,6 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-  },
 
   /* Configure projects for major browsers */
   projects: [
@@ -37,42 +30,24 @@ export default defineConfig({
       name: "chrome",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
+  timeout: 10000,
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm dev",
-    url: "http://127.0.0.1:3000",
-    reuseExistingServer: !process.env.CI,
+    command: "pnpm dev --port 3001",
+    url: "http://localhost:3001",
+    stdout: "pipe",
+    env: {
+      DB_NAME: "playwright-test",
+      NEXT_PUBLIC_APP_URL: "http://localhost:3001",
+    },
+    timeout: 30000,
+    // reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+    baseURL: "http://localhost:3001",
   },
 });
--- tests/integration/test-1.spec.ts
+++ tests/integration/test-1.spec.ts
@@ -1,25 +1,84 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
+import "dotenv/config";
+import clientPromise from "../../utils/mongodb";
+
+test.beforeAll(async () => {
+  const dbName = "playwright-test";
+  const client = (await clientPromise).db(dbName);
+  await client.dropDatabase();
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+  if (testInfo.status !== testInfo.expectedStatus) {
+    // Get a unique place for the screenshot.
+    const screenshotPath = testInfo.outputPath(`failure.png`);
+    // Add it to the report.
+    testInfo.attachments.push({
+      name: "screenshot",
+      path: screenshotPath,
+      contentType: "image/png",
+    });
+    // Take the screenshot itself.
+    await page.screenshot({ path: screenshotPath, timeout: 5000 });
+  }
+});
 
 // login in before all tests
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await page.waitForURL("http://localhost:3000/auth/login");
+  await page.goto("/");
+  await page.waitForURL(`/auth/login`);
   await page.getByLabel("Email *").click();
   await page.getByLabel("Email *").fill("test@mail.com");
   await page.getByLabel("Password *").click();
   await page.getByLabel("Password *").fill("password");
   await page.getByRole("button", { name: "Login" }).click();
-  await page.waitForURL("http://localhost:3000/tasks");
+  await page.waitForURL(`/tasks`);
 });
 
 test("create project", async ({ page }) => {
-  await page.getByRole("button", { name: "Create new Team" }).click();
+  await page.getByRole("button", { name: "Create new project" }).click();
   await page.getByPlaceholder("Team Name").click();
   await page.getByPlaceholder("Team Name").fill("Test Project");
-  await page.getByRole("button", { name: "Create new team" }).click();
-  await page.waitForURL("http://localhost:3000/teams/*-*-*-*-*");
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.waitForURL(`/teams/*-*-*-*-*`);
+});
 
-  // await expect(
-  //   page.getByRole("button", { name: "Test Project", exact: true })
-  // ).toBeVisible();
+test("create task", async ({ page }) => {
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.getByPlaceholder("Team Name").click();
+  await page.getByPlaceholder("Team Name").fill("Test Project");
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.waitForURL(`/teams/*-*-*-*-*`);
+});
+
+test("edit task", async ({ page }) => {
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.getByPlaceholder("Team Name").click();
+  await page.getByPlaceholder("Team Name").fill("Test Project");
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.waitForURL(`/teams/*-*-*-*-*`);
+});
+
+test("create subtask", async ({ page }) => {
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.getByPlaceholder("Team Name").click();
+  await page.getByPlaceholder("Team Name").fill("Test Project");
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.waitForURL(`/teams/*-*-*-*-*`);
+});
+
+test("link subtask", async ({ page }) => {
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.getByPlaceholder("Team Name").click();
+  await page.getByPlaceholder("Team Name").fill("Test Project");
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.waitForURL(`/teams/*-*-*-*-*`);
+});
+
+test("delete task", async ({ page }) => {
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.getByPlaceholder("Team Name").click();
+  await page.getByPlaceholder("Team Name").fill("Test Project");
+  await page.getByRole("button", { name: "Create new project" }).click();
+  await page.waitForURL(`/teams/*-*-*-*-*`);
 });
